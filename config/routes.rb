3b2153c78f3a5500c478b35016fb15186d615a6b
shota520 <hirata.shota.infratop@gmail.com>
2020-03-12 13:17:55 +0900

    Initial Commit Message

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+
+
+  root:to => 'books#top'
+
+  get 'books' => "books#index",as:'books'
+
+  get 'books/:id'  => "books#show",as:'book'
+
+  get 'books/:id/edit'  => "books#edit",as:'edit_book'
+
+  post 'books' => "books#create"
+
+  patch 'books/:id' => 'books#update'
+
+  delete 'books/:id' => 'books#destroy',as:'destroy_book'
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
